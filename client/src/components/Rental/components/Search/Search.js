bbbbc105a6535557573bb7fa5a2d9ecca6e23be4
Dheeraj Rao <dheeraj@shararavideo.com>
2022-09-13 22:29:51 +0530

    third

--- client/src/components/Rental/components/Search/Search.js
+++ client/src/components/Rental/components/Search/Search.js
@@ -4,11 +4,12 @@ import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { Container } from "@mui/system";
 
 const Search = () => {
   return (
     <Box>
-      <Box marginBottom={4}>
+      <Box align={"center"} marginBottom={4}>
         <Typography
           variant="h5"
           data-aos={"fade-up"}
@@ -27,15 +28,16 @@ const Search = () => {
           align={"center"}
           data-aos={"fade-up"}
         >
-          After 3 days all of your offers will arrive and you will have another
-          7 days to select your new company.
-          <br />
-          We make sure to include all the amenities and niceties that a growing
-          startup could possibly need.
+          We usually get back to you in 48 to 72 hours.
+          <Box marginTop={2}>
+            <Button variant="contained" color="primary" size="large">
+              Enquire Now
+            </Button>
+          </Box>
         </Typography>
       </Box>
       <Grid container spacing={4}>
-        <Grid item xs={8} sm={4}>
+        <Grid item xs={6} sm={3}>
           <Box
             padding={{ xs: 3, sm: 6 }}
             width={1}
@@ -50,15 +52,17 @@ const Search = () => {
               flexDirection={{ xs: "column", sm: "row" }}
             >
               <Box>
-                <Typography fontWeight={700} variant={"h6"} gutterBottom>
-                  I share a room
+                <Typography fontWeight={700} gutterBottom>
+                  The Right Advice
                 </Typography>
-                <Typography>Over 20,000 users searching a place</Typography>
+                <Typography>
+                  Our team is an expert at matching you with right solution.
+                </Typography>
               </Box>
             </Box>
           </Box>
         </Grid>
-        <Grid item xs={8} sm={4}>
+        <Grid item xs={6} sm={3}>
           <Box
             padding={{ xs: 3, sm: 6 }}
             width={1}
@@ -73,15 +77,18 @@ const Search = () => {
               flexDirection={{ xs: "column", sm: "row" }}
             >
               <Box>
-                <Typography fontWeight={700} variant={"h6"} gutterBottom>
-                  I share a room
+                <Typography fontWeight={700} gutterBottom>
+                  Trusted For Excellence
                 </Typography>
-                <Typography>Over 20,000 users searching a place</Typography>
+                <Typography>
+                  We've been recognized for our high rate of customer
+                  satisfaction.
+                </Typography>
               </Box>
             </Box>
           </Box>
         </Grid>
-        <Grid item xs={8} sm={4}>
+        <Grid item xs={6} sm={3}>
           <Box
             padding={{ xs: 3, sm: 6 }}
             width={1}
@@ -96,10 +103,38 @@ const Search = () => {
               flexDirection={{ xs: "column", sm: "row" }}
             >
               <Box>
-                <Typography fontWeight={700} variant={"h6"} gutterBottom>
-                  I need a place
+                <Typography fontWeight={700} gutterBottom>
+                  Compare The Best
                 </Typography>
-                <Typography>Over 20,000 users searching a place</Typography>
+                <Typography>
+                  We only compare Market Leaders for Service Quality.
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Grid>
+
+        <Grid item xs={6} sm={3}>
+          <Box
+            padding={{ xs: 3, sm: 6 }}
+            width={1}
+            component={Card}
+            boxShadow={4}
+            data-aos="fade-up"
+          >
+            <Box
+              display={"flex"}
+              justifyContent={"space-between"}
+              alignItems={{ xs: "flex-start", sm: "center" }}
+              flexDirection={{ xs: "column", sm: "row" }}
+            >
+              <Box>
+                <Typography fontWeight={700} gutterBottom>
+                  High ROI
+                </Typography>
+                <Typography>
+                  Our Customized Solutions bring you the best value.
+                </Typography>
               </Box>
             </Box>
           </Box>
